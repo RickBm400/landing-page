c016b230354f60d81cae19c9e5ad67c913cc4f5d
RickBm400 <rjbm29@gmail.com>
2024-03-01 01:56:15 -0500

    feat: new hello world

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
+// project fonts
+import { barlow } from "./utils/fonts";
+import "./sass/global.sass"
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="es">
+        <html lang="es" className={barlow.className}>
             <body>
-                <header>
-                    ola
-                </header>
                 <main>{children}</main>
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import variables from "./sass/variables.module.sass";
+import { bebas } from "./utils/fonts";
+
+const name = "Hola mundo";
+
 export default function Home() {
-    return (
-        <>
-            olamundo
-        </>
-    )
+  return (
+    <div
+      className="w-screen flex justify-center items-center h-screen white-text"
+      style={{ backgroundColor: variables.bgColor }}
+    >
+      <div
+        className={`${bebas.className} w-[90%] h-[90%] font-[100] text-[10rem] rounded-[1rem] flex justify-center items-center`}
+        style={{
+          backgroundColor: variables.bgColorGreen,
+          color: variables.bgColor,
+        }}
+      >
+        {name}
+      </div>
+    </div>
+  );
 }
