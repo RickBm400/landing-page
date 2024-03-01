7ca1a87422feee2134da1de750debb81e0f60da6
RickBm400 <rjbm29@gmail.com>
2024-03-01 01:55:49 -0500

    optimized fonts & global/variables sass modified

---
+++ app/utils/fonts.ts
@@ -0,0 +1,13 @@
+import { Barlow, Bebas_Neue } from 'next/font/google'
+
+export const barlow = Barlow({
+    subsets: ['latin'],
+    weight: '400',
+    display: 'swap'
+})
+
+export const bebas = Bebas_Neue({
+    weight: '400',
+    display: 'swap',
+    subsets: ['latin']
+})
