6fde27179d757eee4f7d5b7e5274c60effbe38a5
RickBm400 <rjbm29@gmail.com>
2024-03-04 02:15:57 -0500

    feat: paper dashboard grid layout section

--- app/paperdash/page.tsx
+++ app/paperdash/page.tsx
@@ -1,22 +1,27 @@
 "use client";
-import "sass/paperdash.sass";
+import "./paperdash.sass";
 import Grid from "@mui/material/Unstable_Grid2";
 
 export default function PaperDash() {
   return (
     <>
-      <Grid container id="container" className="p-[1rem]" spacing={0}>
-        <Grid container xs={12} className={`col-h-20`} spacing={0}>
-          <Grid xs={6}>Hola</Grid>
+      <Grid container id="container" spacing={0}>
+        <Grid
+          container
+          xs={12}
+          className={`b-top b-left b-right b-bottom h-[20%]`}
+          spacing={0}
+        >
+          <Grid xs={6}>99px</Grid>
           <Grid xs={6}>Mundo</Grid>
         </Grid>
-        <Grid xs={6} className={`col-h-60`}>
+        <Grid xs={6} className={`b-left b-right b-bottom h-[60%]`}>
           ols
         </Grid>
-        <Grid xs={6} className={`col-h-60`}>
+        <Grid xs={6} className={`b-right b-bottom h-[60%]`}>
           ola
         </Grid>
-        <Grid xs={12} className={`col-h-20`}>
+        <Grid xs={12} className={`b-left b-right b-bottom h-[20%]`}>
           12
         </Grid>
       </Grid>
