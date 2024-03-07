349523d879b942d87f2a2a76d0d55fc55efb0f06
RickBm400 <rjbm29@gmail.com>
2024-03-06 19:53:50 -0500

    Kode mono font added & paper dash header

--- app/paperdash/page.tsx
+++ app/paperdash/page.tsx
@@ -1,25 +1,77 @@
 'use client';
+import { Kode_mono } from '../utils/fonts';
 import './paperdash.sass';
 import Grid from '@mui/material/Unstable_Grid2';
 
 export default function PaperDash() {
+  interface contact {
+    phone_number: string;
+    email: string;
+    location: string;
+  }
+  const links = [
+    {
+      url: '',
+      name: 'About',
+    },
+    {
+      url: '',
+      name: 'Work',
+    },
+    {
+      url: '',
+      name: 'What i do',
+    },
+    {
+      url: '',
+      name: 'Contact me',
+    },
+  ];
+
+  const contacts: contact = {
+    phone_number: '99999999',
+    email: 'rjbm29@gmail.com',
+    location: 'B/quilla',
+  };
+
   return (
     <>
-      <Grid container id="container" spacing={0}>
+      <Grid
+        container
+        id="container"
+        spacing={0}
+        className={Kode_mono.className}
+      >
         <Grid
           container
           xs={12}
           className={`b-top b-left b-right b-bottom h-[20%]`}
           spacing={0}
         >
-          <Grid xs={6}>99px</Grid>
           <Grid xs={6}>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
+            <h1>This is the header</h1>
+          </Grid>
+
+          <Grid xs={6} container spacing={0}>
+            <Grid xs={5}>
+              <ul>
+                {links.map((link) => {
+                  return <li>{link.name}</li>;
+                })}
+              </ul>
+            </Grid>
+            <Grid xs={7}>
+              {Object.keys(contacts).map((key: string) => {
+                return (
+                  <>
+                    <div className="contact_data flex justify-between">
+                      <span>{key.replace('_', ' ')}</span>
+                      <span>{contacts[key as keyof typeof contacts]}</span>
+                    </div>
+                  </>
+                );
+              })}
+            </Grid>
           </Grid>
         </Grid>
         <Grid xs={6} className={`b-left b-right b-bottom h-[60%]`}>
--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -1,4 +1,8 @@
-import { Barlow, Bebas_Neue } from 'next/font/google';
+import localFont from 'next/font/local'
+
+import { Barlow, Bebas_Neue, } from 'next/font/google';
+
+export const Kode_mono = localFont({ src: "../../public/fonts/KodeMono-VariableFont_wght.ttf", weight: '500', style: 'normal' })
 
 export const barlow = Barlow({
   subsets: ['latin'],
