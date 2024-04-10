b6616c0b0b0570b878e7abb187a2ed500a832e83
RickBm400 <rjbm29@gmail.com>
2024-04-10 17:05:06 -0500

    feat: new middleware file

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 'use client';
 import variables from './sass/variables.module.sass';
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { useRouter } from 'next/navigation';
 import { Button } from '@mui/material';
 import { buttonTheme, button2theme } from './utils/materialUi/theme';
 import { ThemeProvider } from '@emotion/react';
 
 export default function Home() {
+  const router = useRouter();
+  console.log(router.forward());
+
   return (
     <>
       <Grid
-        xs={12}
         spacing={0}
         container
         className="h-screen"
         style={{ backgroundColor: variables.bgColor, color: 'white' }}
       >
         <Grid xs={7} className="flex justify-center items-center h-screen">
-          olamundo {variables.bgColor}
+          olamundo
         </Grid>
         <Grid xs={5} className="flex justify-center items-center h-[100vh]">
           <section
