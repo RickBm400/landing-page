c058fe894bb60ea053dee67cac10e29bbce929a0
RickBm400 <rjbm29@gmail.com>
2024-06-18 22:01:58 -0500

    feat: whatever it is

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,38 @@
 'use client';
 import variables from './sass/variables.module.sass';
 import Grid from '@mui/material/Unstable_Grid2';
-import { useRouter } from 'next/navigation';
-import { Button } from '@mui/material';
-import { buttonTheme, button2theme } from './utils/materialUi/theme';
-import { ThemeProvider } from '@emotion/react';
+import Image from 'next/image';
+import land from 'utils/img/landscape-large.jpg';
+import './styles.sass';
 
 export default function Home() {
-  const router = useRouter();
-  console.log(router.forward());
-
   return (
-    <>
+    <div className="landing_container">
       <Grid
         spacing={0}
         container
         className="h-screen"
-        style={{ backgroundColor: variables.bgColor, color: 'white' }}
+        style={{ backgroundColor: variables.blackVariable, color: 'white' }}
       >
         <Grid xs={7} className="flex justify-center items-center h-screen">
           olamundo
         </Grid>
         <Grid xs={5} className="flex justify-center items-center h-[100vh]">
-          <section
-            className="h-[95%] w-[90%]"
-            style={{ backgroundColor: variables.bgColorGreen }}
-          >
-            ola
+          <section className="h-[95%] w-[90%]">
+            <Image style={{ height: '100%' }} src={land} alt="crop" />
           </section>
         </Grid>
       </Grid>
+      <section className="h-screen"></section>
+      <footer
+        className="h-[16rem]"
+        style={{ backgroundColor: variables.yellowVariable }}
+      ></footer>
       {/* <ThemeProvider theme={true ? buttonTheme : button2theme}>
         <Button href="/playground" variant="contained">
           Go to playground
         </Button>
       </ThemeProvider> */}
-    </>
+    </div>
   );
 }
